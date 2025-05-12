9fa6430070bffe86fdb4b0a04a0d5e8969963f69
Данила Беляков <danilabelakov@MacBook-Air-Danila.local>
2025-05-12 14:08:01 +0400

    error handler tests

--- library/src/test/java/com/github/danbel/spring/firebase/appcheck/configs/FirebaseCheckAppConf.java
+++ library/src/test/java/com/github/danbel/spring/firebase/appcheck/configs/FirebaseCheckAppConf.java
@@ -5,7 +5,6 @@ import com.github.danbel.spring.firebase.appcheck.model.properties.FirebaseAppCh
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.http.HttpMethod;
 
 @Configuration
 public class FirebaseCheckAppConf {
@@ -13,7 +12,6 @@ public class FirebaseCheckAppConf {
     @Bean
     @Primary
     public FirebaseAppCheckProperties firebaseAppCheckProperties(FirebaseAppCheckProperties properties) {
-        properties.addRoute(HttpMethod.POST, "/route");
         properties.setTokenVerifierService(new FirebaseAppCheckTokenVerifierServiceMock());
         return properties;
     }
